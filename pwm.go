632b45b7dd5fb7bcf5210a086c61b5228dd08752
Klaus Tockloth <klaus.tockloth@googlemail.com>
2024-02-25 09:24:18 +0100

    Initial commit

--- pwm.go
+++ pwm.go
@@ -3,13 +3,31 @@ Purpose:
 - Hardware generated Pulse Width Modulation (PWM) for Raspberry Pi.
 
 Releases:
-- v1.0.0 - 2024/02/04: initial release
+- v1.0.0 - 2024/02/25: initial release
 
 Author:
 - Klaus Tockloth
 
 Copyright:
 - © 2024 | Klaus Tockloth
+
+License:
+- MIT license
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the Software), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+The software is provided 'as is', without warranty of any kind, express or implied, including
+but not limited to the warranties of merchantability, fitness for a particular purpose and
+noninfringement. In no event shall the authors or copyright holders be liable for any claim,
+damages or other liability, whether in an action of contract, tort or otherwise, arising from,
+out of or in connection with the software or the use or other dealings in the software.
 
 Contact:
 - klaus.tockloth@googlemail.com
